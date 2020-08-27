3151d5f289065097ed7ddfdb1620c7358a39e64c
Kemal Zebari <kemalzebari@Kemals-MBP.fios-router.home>
2020-08-26 23:34:16 -0700

    Minor refactoring and error handling implemented

--- proto/java/relations/src/RelationChainer.java
+++ proto/java/relations/src/RelationChainer.java
@@ -1,14 +1,20 @@
 package proto.java.relations.src;
 
-//import java.util;
 import java.util.Stack;
 import java.util.UnknownFormatConversionException;
 
 public class RelationChainer {
 
-//! Little to no error handling as of yet for this method
+// * *
+  // Instance Methods
+// * *
+
+//! Little to no proper error handling currently for this method
+  // Currently, if an error is caused by the end user, this method will
+  // just return a false value (i.e 1 <<= 2 >= 0 , this would be a constitute)
   public boolean relation(String dummy, String input, Integer... prms) {
     boolean output = true;
+    boolean opr_exists = false;
     StringBuilder operator = null;
     String formatted = null;
 
@@ -18,17 +24,23 @@ public class RelationChainer {
     } catch(UnknownFormatConversionException e) {
       e.printStackTrace();
     } //* If this passes, 'input' has correct format specifiers
-    System.out.println(formatted);
+
+    // Below displays the formatted string in the console if given a
+      // special 'dummy' value of "show"
+    if(dummy.equals("show")) {
+      System.out.println(formatted);
+    }
 
       //* Is the string empty and is there varargs arguements
-    if(!formatted.isEmpty() && prms[0] != null) {
-        // If there exists some relation character at the beginning
+    if(!formatted.isEmpty() && prms.length >= 0) {
+        // If there exists some relation character at the beginning,
+          // the entire expression is invalid
       if(isRelationalOp(Character.toString(formatted.charAt(0))) ) {
         return false; // !!! Error handling requried
       }
 
       int j = 0; // represents prms indices
-      String curr_element; // used to lessen the use of methods for better readability
+      String curr_element; // used to lessen the use of string methods for better readability
       for(int i = 0; i < formatted.length(); i++) {
         curr_element = Character.toString(formatted.charAt(i));
 
@@ -36,22 +48,24 @@ public class RelationChainer {
         if(Character.isWhitespace(formatted.charAt(i)) ) {
           continue;
         }
-        // Is the current element a "relational opeartor"
+        // Is the current element a "relational operator"
         if(isRelationalOp(curr_element)) {
           if(operator == null) {
             operator = new StringBuilder(curr_element);
           }
-          else if(operator.length() > 3) {
+          else if(operator.length() > 2) {
             return false; // !!! Error handling required (could use break??)
           }
           else {
-            operator.append(curr_element); //update stack top
-
+            operator.append(curr_element); // applies to >=, <=, and ==
           }
         }
 
-        else if(isDigit(formatted.charAt(i))) { // If current is a number
-          if(operator == null) continue;
+        else if(isDigit(formatted.charAt(i))) { // If current ele is a number
+          if(operator == null)
+            continue;
+
+          opr_exists = true; // operators exist within the expression (EH)
           if(j+1 < prms.length) {
             output = output & calculateBinaryRelation(operator.toString(),
             prms[j], prms[++j]);
@@ -59,29 +73,22 @@ public class RelationChainer {
           }
         }
         else
-          return false; // the mathematical expression contains invalid characters
+          return false; // !!! the mathematical expression contains invalid characters
       }
-      //* If the StringBuilder is not null, the mathematical expression is invalid
+      //* If operator is not null, the mathematical expression is invalid
       if(operator != null) return false; // !!! Error handling required
+      if(!opr_exists) return false; // !!! Error handling required
     }
 
     //* No formatted string or no varargs arguements
-    else
+    else {
       return false;
-
+    }
     return output;
   }
 
-  //* This is a overloaded method in current development
-    // It may essentially bypass the need of a formatted string
-  public boolean relation(String input, Integer... prms) {
-    StringBuilder mutable_input = new StringBuilder(input);
-
-    return false;
-  }
-
 // * *
-// Helper Methods
+  // Helper Methods
 // * *
 
     //* isRelationalOp checks if the formatted string is an relational operator
@@ -90,7 +97,7 @@ public class RelationChainer {
       case ">":
       case "<":
       case "=": return true;
-        // Only these considered as we are transversing a string linearly
+        // Only these chars considered as we are transversing a string linearly
       default: return false;
     }
   }
@@ -114,7 +121,6 @@ public class RelationChainer {
 
     //* Determines whether a given string is a valid number
   private boolean isDigit(Character in) {
-    //if(in == null) return false;
     return Character.isDigit(in);
   }
 
